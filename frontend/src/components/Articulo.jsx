c0555a8c598279e2dacc23cdcf344243f2f23f64
nicoard12 <105379302+nicoard12@users.noreply.github.com>
2024-04-24 00:44:17 -0300

    mejorando css de UltimoTrueque

--- frontend/src/components/Articulo.jsx
+++ frontend/src/components/Articulo.jsx
@@ -4,19 +4,19 @@ import routes from '../routes'
 
 function Articulo ({articulo}){
     return (
-        <div className='articulo-link'>
+        
 
         <Link to={routes.unArticulo} className='link'>
             <div className='articulo'>
-                <img src={articulo.src} alt="" />
+                <img src={articulo.src} alt="" className='imagenArt'/>
                 <div className='articulo-contenido'>
-                    <h2>{articulo.titulo}</h2>
-                    <p>{articulo.descripcion}. Definir un maximo de caracteres.</p>
+                    <h2 className='tituloArt'>{articulo.titulo}</h2>
+                    <p className='descripcionArt'>{articulo.descripcion}. Definir un maximo de caracteres.</p>
                     <span className='span'>${articulo.precio}</span>
                 </div>
             </div>
         </Link>
-        </div>
+        
     )
 }
 
