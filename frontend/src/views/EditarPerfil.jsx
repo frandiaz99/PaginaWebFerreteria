423607ecfba72ba2c44811e4adfeb3763fa0f1dd
frandiaz99 <fdmalbran@gmail.com>
2024-05-04 16:09:27 -0300

    editarperfil

---
+++ frontend/src/views/EditarPerfil.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+function EditarPerfil() {
+
+    const [usuario, setUsuario] = useState();
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user/getSelf',
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/JSON",
+                }, credentials: "include"
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Hubo un problema al obtener los articulos');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                setUsuario(data.User)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, [])
+
+    return (
+        
+    )
+
+
+}
+
+export default EditarPerfil
