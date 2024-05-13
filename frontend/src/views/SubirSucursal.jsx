74c7f1528d3a622db526b95339d0ba8b9db7cb01
frandiaz99 <fdmalbran@gmail.com>
2024-05-12 23:21:27 -0300

    subirsucursal completo

--- frontend/src/views/SubirSucursal.jsx
+++ frontend/src/views/SubirSucursal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/SubirSucursal.css'
+import Modal from "../components/Modal";
 
 function SubirSucursal() {
   const [datos, setDatos] = useState({
@@ -10,6 +11,7 @@ function SubirSucursal() {
     telefono: ''
   })
 
+  const [subirSucursal, setSubirSucursal] = useState(false)
   const [imagen, setImagen] = useState({ foto: "" })
 
   const handleChange = (e) => {
@@ -29,7 +31,7 @@ function SubirSucursal() {
 
     fetch("http://localhost:5000/sucursal/newSucursal", {
       method: "POST",
-      headers: { "Content-Type": "application/JSON" },
+      //headers: { "Content-Type": "application/JSON" },
       body: formData,
       credentials: "include"
     })
@@ -48,45 +50,60 @@ function SubirSucursal() {
         console.error("Error en la creacion de sucursal:", error.message);
       });
   }
+
+
+  const handleOk = () => {
+    setSubirSucursal(false)
+    window.location.href = routes.adminSucursales
+  };
+
   return (
     <main className='main'>
       <form className='formSubirSucursal' onSubmit={handleSubirSucursal} encType="multipart/form-data">
-        <div className='div-subirSucursal'>
-          <label htmlFor="nombre">Nombre de la sucursal</label>
-          <input type="text" name='nombre' className='inputSubirSucursal' onChange={handleChange} />
-        </div>
-        <div className='div-subirSucursal'>
-          <label htmlFor="provincia">Provincia de la sucursal</label>
-          <input type="text" name='provincia' className='inputSubirSucursal' onChange={handleChange} />
-        </div>
-        <div className='div-subirSucursal'>
-          <label htmlFor="ciudad">Ciudad de la sucursal</label>
-          <input type="text" name='ciudad' className='inputSubirSucursal' onChange={handleChange} />
-        </div>
-        <div className='div-subirSucursal'>
-          <label htmlFor="direccion">Direccion de la sucursal</label>
-          <input type="text" name='direccion' className='inputSubirSucursal' onChange={handleChange} />
-        </div>
-        <div className='div-subirSucursal'>
-          <label htmlFor="telefono">Telefono de la sucursal</label>
-          <input type="text" name='telefono' className='inputSubirSucursal' onChange={handleChange} />
-        </div>
+        <div className='div-subirsucursal-inputs'>
+          <div className='div-subirsucursal-fila'>
+            <div className='div-subirSucursal'>
+              <label htmlFor="nombre">Nombre de la sucursal</label>
+              <input type="text" name='nombre' className='inputSubirSucursal' onChange={handleChange} />
+            </div>
+            <div className='div-subirSucursal'>
+              <label htmlFor="provincia">Provincia de la sucursal</label>
+              <input type="text" name='provincia' className='inputSubirSucursal' onChange={handleChange} />
+            </div>
+            <div className='div-subirSucursal'>
+              <label htmlFor="ciudad">Ciudad de la sucursal</label>
+              <input type="text" name='ciudad' className='inputSubirSucursal' onChange={handleChange} />
+            </div>
+          </div>
+          <div className='div-subirsucursal-fila'>
+            <div className='div-subirSucursal'>
+              <label htmlFor="direccion">Direccion de la sucursal</label>
+              <input type="text" name='direccion' className='inputSubirSucursal' onChange={handleChange} />
+            </div>
+            <div className='div-subirSucursal'>
+              <label htmlFor="telefono">Telefono de la sucursal</label>
+              <input type="text" name='telefono' className='inputSubirSucursal' onChange={handleChange} />
+            </div>
 
-        <div className='div-subirSucursañ'>
-          <input id='input-fott' type="file" accept=".png, .jpg, .jpeg" multiple name="foto"
-            onChange={e => {
-              console.log({ "name": e.target.name })
-              console.log(e.target.files[0])
-              setImagen({ [e.target.name]: e.target.files[0] })
-              console.log("foto", foto)
+            <div className='div-subirSucursal'>
+              <input className='input-foto' id='input-foto' type="file" accept=".png, .jpg, .jpeg" multiple name="foto"
+                onChange={e => {
+                  console.log({ "name": e.target.name })
+                  console.log(e.target.files[0])
+                  setImagen({ [e.target.name]: e.target.files[0] })
+                  console.log("foto", imagen.foto)
 
-            }} />
+                }} />
+            </div>
+          </div>
         </div>
 
         <div className='div-botonSubirSucursal'>
-          <button type="submit" className='registrarse'>Subir sucursal </button>
+          <button type="submit" className='boton-subir-sucursal'>Subir sucursal </button>
         </div>
       </form>
+      <Modal texto={'La sucursal se ha creado con exito.'}
+        confirmacion={subirSucursal} setConfirmacion={setSubirSucursal} handleYes={handleOk} ok={true} />
     </main>
   )
 }
