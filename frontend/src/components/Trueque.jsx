420a3ca853aaeef03e8100b18c8dad11a9f7483e
frandiaz99 <fdmalbran@gmail.com>
2024-05-26 10:50:46 -0300

    truequespendientes

--- frontend/src/components/Trueque.jsx
+++ frontend/src/components/Trueque.jsx
@@ -1,68 +1,79 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Trueque.css'
 import Modal from '../components/Modal'
 
-function Trueque({trueque, pendiente}) {
-  const [modalCancelar, setModalCancelar]= useState(false)
+function Trueque({ trueque, pendiente }) {
+  const [modalCancelar, setModalCancelar] = useState(false)
 
-  const handleCancelar= () =>{
+  const handleCancelar = () => {
     setModalCancelar(true)
   }
 
-  const handleYes= () =>{
+  const handleYes = () => {
     //fetch para cancelar trueque
     setModalCancelar(false)
     window.location.reload()
   }
+
+  function mostrar(trueque) {
+    console.log("truequeeeee", trueque)
+    if (trueque.articulo_publica == null) {
+      console.log("soy un NULL")
+    }
+
+    return trueque.dni1
+  }
+
+
   return (
     <div className='unTrueque'>
 
-        <div className='contenido-unTrueque'>
+      <div className='contenido-unTrueque'>
 
-          <div className='informacion-unTrueque'>
+        <div className='informacion-unTrueque'>
 
-            <div className='usuario-unTrueque'>
-              <h3 className='dni-unTrueque'>{trueque.dni1}</h3>
-              <p className='nombre-unTrueque'>{trueque.nombre1} </p>
-            </div>
-
-            <div className='art-unTrueque'>
-              <h4>{trueque.art1}</h4>
-            </div>
-
-            <div className='divImagen-unTrueque'> <img src="truequeicono.avif" alt="" className='imagenTrueque-unTrueque' /></div>
-            
-            <div className='art-unTrueque'>
-              <h4>{trueque.art2}</h4>
-            </div>
-
-            <div className='usuario-unTrueque'>
-              <h3 className='dni-unTrueque'>{trueque.dni2}</h3>
-              <p className='nombre-unTrueque'>{trueque.nombre2}</p>
-            </div>
-
+          <div className='usuario-unTrueque'>
+            <h3 className='dni-unTrueque'>{mostrar(trueque)}</h3>
+            <p className='nombre-unTrueque'>{trueque.nombre1} </p>
           </div>
 
-          <div className='fecha-unTrueque'>
-            <span>{trueque.fecha}</span>
+          <div className='art-unTrueque'>
+            <h4>{trueque.art1}</h4>
+          </div>
+
+          <div className='divImagen-unTrueque'> <img src="truequeicono.avif" alt="" className='imagenTrueque-unTrueque' /></div>
+
+          <div className='art-unTrueque'>
+            <h4>{trueque.art2}</h4>
+          </div>
+
+          <div className='usuario-unTrueque'>
+            <h3 className='dni-unTrueque'>{trueque.dni2}</h3>
+            <p className='nombre-unTrueque'>{trueque.nombre2}</p>
           </div>
 
         </div>
 
+        <div className='fecha-unTrueque'>
+          <span>{trueque.fecha}</span>
+        </div>
 
-        <div className='opciones-unTrueque'>
-          {pendiente
+      </div>
+
+
+      <div className='opciones-unTrueque'>
+        {pendiente
           ?
-            <div className='cancelar_efectivizar'>
-              <button className='botonUnTrueque' onClick={handleCancelar}>Cancelar</button>
-              <button className='botonUnTrueque'>Efectivizar</button>
-            </div>
+          <div className='cancelar_efectivizar'>
+            <button className='botonUnTrueque' onClick={handleCancelar}>Cancelar</button>
+            <button className='botonUnTrueque'>Efectivizar</button>
+          </div>
           :
-            <div className='divRegistrarVenta'>
-              <button className='botonUnTrueque'>Registrar venta</button>
-            </div> }
+          <div className='divRegistrarVenta'>
+            <button className='botonUnTrueque'>Registrar venta</button>
+          </div>}
 
-        </div>
+      </div>
       <Modal texto={'¿Estás seguro que querés cancelar el trueque?'} confirmacion={modalCancelar} setConfirmacion={setModalCancelar} handleYes={handleYes} ok={false} />
     </div>
   )
--- frontend/src/views/PrincipalAdminYEmple.jsx
+++ frontend/src/views/PrincipalAdminYEmple.jsx
@@ -7,38 +7,65 @@ import trueques from '../data/trueques.json'
 
 
 function PrincipalAdminYEmple() {
-  const usuarioActual= JSON.parse(localStorage.getItem('user'))
-  const [verPendientes, setVerPendientes]= useState(true)
-  const [truequesPendientes, setTruequesPendientes]= useState([])
-  const [truequesCompletados, setTruequesCompletados]= useState([])
-  const titulo_pendientes_ref= useRef(null)
-  const titulo_completados_ref= useRef(null)
+  const usuarioActual = JSON.parse(localStorage.getItem('user'))
+  const [verPendientes, setVerPendientes] = useState(true)
+  const [truequesPendientes, setTruequesPendientes] = useState([])
+  const [truequesCompletados, setTruequesCompletados] = useState([])
+  const titulo_pendientes_ref = useRef(null)
+  const titulo_completados_ref = useRef(null)
 
-  const handlePendientes= () =>{
+  useEffect(() => {
+    fetch("http://localhost:5000/trueque/getPendientes", {
+      method: "GET",
+      headers: { "Content-Type": "application/JSON" },
+      credentials: "include"
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(data => {
+            throw new Error(JSON.stringify({ message: data.message, status: data.status }));
+          })
+        }
+        return response.json();
+      })
+      .then(data => {
+        setTruequesPendientes(data.data)
+        console.log("donde tan los trueques, aca tannnnn: ", truequesPendientes)
+        console.log("data1 -> ", data)
+        console.log("data2:", data.data)
+        console.log("cuanto te mide?: ", truequesPendientes, length)
+        setVerPendientes(true)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
+  }, []);
+
+  const handlePendientes = () => {
     titulo_completados_ref.current.style.color = 'rgb(170, 170, 170)'
     titulo_pendientes_ref.current.style.color = 'black'
     setVerPendientes(true)
   }
 
-  const handleCompletados= () =>{
+  const handleCompletados = () => {
     titulo_completados_ref.current.style.color = 'black'
     titulo_pendientes_ref.current.style.color = 'rgb(170, 170, 170)'
     setVerPendientes(false)
   }
 
-  const obtenerPendientes=(trueques)=>{
-    if (trueques){
+  const obtenerPendientes = (trueques) => {
+    if (trueques) {
       console.log(usuarioActual)
-      if (usuarioActual.rol == 2){
+      if (usuarioActual.rol == 2) {
         return trueques.filter(t => t.completado == false && t.sucursal == usuarioActual.puntos) //comparo con puntos porque no existe sucursal todavia en mongo
       }
       return trueques.filter(t => t.completado == false)
     }
     return []
   }
-  const obtenerCompletados=(trueques)=>{
-    if (trueques){
-      if (usuarioActual.rol == 2){
+  const obtenerCompletados = (trueques) => {
+    if (trueques) {
+      if (usuarioActual.rol == 2) {
         return trueques.filter(t => t.completado == true && t.sucursal == usuarioActual.puntos)
       }
       return trueques.filter(t => t.completado == true)
@@ -46,11 +73,11 @@ function PrincipalAdminYEmple() {
     return []
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     titulo_completados_ref.current.style.color = 'rgb(170, 170, 170)'
     titulo_pendientes_ref.current.style.color = 'black'
     //fetch para obtener todos los trueques
-    setTruequesPendientes(obtenerPendientes(trueques)) //import json temporal
+    //setTruequesPendientes(obtenerPendientes(trueques)) //import json temporal
     setTruequesCompletados(obtenerCompletados(trueques))
   }, [])
 
@@ -58,22 +85,34 @@ function PrincipalAdminYEmple() {
     <main className='main'>
       <div className='principal_admin_emple'>
 
-          <div className='pendientes_y_completados-principal_admin_emple'>
-            <h4 className='titulo-pendientes_y_completados' onClick={handlePendientes} ref={titulo_pendientes_ref}>Trueques Pendientes</h4>
-            <h4 className='titulo-pendientes_y_completados' onClick={handleCompletados} ref={titulo_completados_ref}>Trueques Completados</h4>
-          </div>
-       
-
-          <div className='filtros_y_trueques-principal_admin_emple'>
-            {verPendientes ? <Buscador/> : <FiltroFecha/>}
-
-            <div className='trueques'>
-              {verPendientes && truequesPendientes.map((t,index) => (<Trueque key={index} trueque={t} pendiente={true}/>))}
-              {!verPendientes && truequesCompletados.map((t,index) => (<Trueque key={index} trueque={t} pendiente={false}/>))}
-            </div>
+        <div className='pendientes_y_completados-principal_admin_emple'>
+          <h4 className='titulo-pendientes_y_completados' onClick={handlePendientes} ref={titulo_pendientes_ref}>Trueques Pendientes</h4>
+          <h4 className='titulo-pendientes_y_completados' onClick={handleCompletados} ref={titulo_completados_ref}>Trueques Completados</h4>
         </div>
 
-       {!verPendientes && <div className='ganancia-principal_admin_emple'>
+
+        <div className='filtros_y_trueques-principal_admin_emple'>
+          {verPendientes ? <Buscador /> : <FiltroFecha />}
+
+          <div className='trueques'>
+            {verPendientes && truequesPendientes.length > 0 ? (
+              truequesPendientes.map((t, index) => (
+                <Trueque key={index} trueque={t} pendiente={true} />
+              ))
+            ) : verPendientes ? (
+              <p> No hay trueques pendientes</p>
+            ) : truequesCompletados.length > 0 ? (
+              truequesCompletados.map((t, index) => (
+                <Trueque key={index} trueque={t} pendiente={false} />
+              ))
+            ) : (
+              <p>No hay trueques completados</p>
+            )}
+
+          </div>
+        </div>
+
+        {!verPendientes && <div className='ganancia-principal_admin_emple'>
           <h3 className='tituloGanancia'>Ganancia</h3>
           <h2 className='ganancia'>$1000</h2>
         </div>}
